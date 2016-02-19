437fa049152b58016648da717852fe55de313c25
Michael <michael.kushniriov@gmail.com>
2016-02-19 12:04:03 +0200

    added unit testing , next - https://developer.apple.com/library/ios/referencelibrary/GettingStarted/DevelopiOSAppsSwift/Lesson7.html#//apple_ref/doc/uid/TP40015214-CH8-SW1 create a table view

---
+++ FoodTracker/FoodTracker/Meal.swift
@@ -0,0 +1,30 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Michael Kushniriov on 19/02/2016.
+//  Copyright © 2016 Michael Kushniriov. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    // MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    // MARK: Initialization
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+        // Initialization should fail if there is no name or if the rating is negative.
+        if name.isEmpty || rating < 0 {
+            return nil
+        }
+    }
+}
