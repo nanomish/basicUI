2f822b3c48dc0550462e889ef37a096d50aaeeea
Michael <michael.kushniriov@gmail.com>
2016-02-24 20:29:38 +0200

    on a check point here https://developer.apple.com/library/ios/referencelibrary/GettingStarted/DevelopiOSAppsSwift/Lesson7.html#//apple_ref/doc/uid/TP40015214-CH8-SW1 (at this text: ) In a storyboard, a table view can be configured to display static data (supplied in the storyboard UI) or dynamic data (supplied by the table view controller logic). The table view defaults to using dynamic data, and because you’ll need to load data in code, this is what you want it to do—you just haven’t implemented that behavior yet. This means that the static content you supplied in the storyboard doesn’t show up at runtime, so you can’t see it when you run the app—until you implement the data model behind it.

---
+++ FoodTracker/FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Michael Kushniriov on 19/02/2016.
+//  Copyright © 2016 Michael Kushniriov. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+}
